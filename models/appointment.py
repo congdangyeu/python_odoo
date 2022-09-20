a0822668fc711a2e7f7e9848fb0154a583b9fc1e
congdangyeu <111550951+congvuviet@users.noreply.github.com>
2022-09-20 22:32:42 +0700

    Update appointment.py

--- models/appointment.py
+++ models/appointment.py
@@ -24,7 +24,7 @@ class HospitalAppointment(models.Model):
 
     def action_confirm(self):
         self.state = 'confirm'
-        print('Test GIT1!!!')
+        print('Test GIT111!!!')
 
     def action_done(self):
         self.state = 'done'
